0bf9e81bb7c8026f7b2068f5453efbe75eddfadc
Jules Fouchy <jules.fouchy@ntymail.com>
2024-08-25 18:18:20 +0200

    ✨ Proper Server class

--- include/serv/serv.hpp
+++ include/serv/serv.hpp
@@ -1,17 +1,60 @@
 #pragma once
-
 #include <functional>
-#include <string_view>
+#include <memory>
+#include <optional>
+#include <string>
+#include <utility>
+#include "CivetServer.h"
 
 namespace serv {
 
-struct Get {};
+class Request {
+public:
+    explicit Request(mg_connection* connection)
+        : _connection{connection}
+    {}
+    auto get(std::string_view param) const -> std::optional<std::string>;
 
-struct Post {};
+private:
+    mg_connection* _connection{};
+};
 
-using RequestHandler = std::function<void(std::string_view)>;
+using RequestCallback = std::function<void(Request const&)>;
 
-void init(RequestHandler callback);
-void shut_down();
+namespace internal {
+
+class CivetRAII { // NOLINT(*special-member-functions)
+public:
+    CivetRAII() { mg_init_library(0); }
+    ~CivetRAII() { mg_exit_library(); }
+};
+
+class Handler : public CivetHandler {
+public:
+    explicit Handler(RequestCallback callback)
+        : _callback{std::move(callback)}
+    {}
+
+    auto handleGet(CivetServer*, mg_connection* connection) -> bool override
+    {
+        _callback(Request{connection});
+        return true;
+    }
+
+private:
+    RequestCallback _callback;
+};
+
+} // namespace internal
+
+class Server {
+public:
+    explicit Server(std::string const& port, std::vector<std::pair<std::string, RequestCallback>> const& callbacks);
+
+private:
+    std::unique_ptr<internal::CivetRAII> _raii{std::make_unique<internal::CivetRAII>()};
+    std::unique_ptr<CivetServer>         _server; // We must not copy a CivetServer because it is buggy (not implemented, and the default behavior is wrong)
+    std::vector<internal::Handler>       _handlers{};
+};
 
 } // namespace serv
--- src/serv.cpp
+++ src/serv.cpp
@@ -1,80 +1,33 @@
-#include "../include/serv/serv.hpp"
-#include <iostream>
-#include "CivetServer.h"
+#include "serv/serv.hpp"
+#include <CivetServer.h>
+#include <stdexcept>
+#include <vector>
 
 namespace serv {
 
-class Handler : public CivetHandler {
-private:
-    RequestHandler _handler;
-
-    bool handleAll(mg_connection* conn)
-    {
-        mg_printf(conn,
-                  "HTTP/1.1 200 OK\r\nContent-Type: "
-                  "text/html\r\n\r\n\r\n");
-        std::string s = "";
-        if (CivetServer::getParam(conn, "param", s))
-        {
-            mg_printf(conn, "<p>param set to %s</p>", s.c_str());
-        }
-        else
-        {
-            mg_printf(conn, "<p>param not set</p>");
-        }
-        return true;
-    }
-
-public:
-    bool handleGet(CivetServer*, mg_connection* conn)
-    {
-        std::string s;
-        if (CivetServer::getParam(conn, "command", s))
-        {
-            _handler(s);
-        }
-        else
-        {
-            _handler("No command");
-        }
-        return handleAll(conn);
-    }
-
-    bool handlePost(CivetServer*, mg_connection* conn)
-    {
-        std::string s;
-        if (CivetServer::getParam(conn, "command", s))
-        {
-            _handler(s);
-        }
-        else
-        {
-            _handler("No command");
-        }
-        return handleAll(conn);
-    }
-
-    explicit Handler(RequestHandler handler)
-        : _handler{handler}
-    {}
-};
-
-void init(RequestHandler handler)
+auto Request::get(std::string_view param) const -> std::optional<std::string>
 {
-    mg_init_library(0);
-
-    static CivetServer server(
-        {"listening_ports", "6002",
-         "num_threads", "1"}
-    );
-
-    static Handler myHandler{handler};
-    server.addHandler("/", myHandler);
+    std::string res{};
+    if (CivetServer::getParam(_connection, param.data(), res))
+        return res;
+    return std::nullopt;
 }
 
-void shut_down()
+Server::Server(std::string const& port, std::vector<std::pair<std::string, RequestCallback>> const& callbacks)
+    : _server{std::make_unique<CivetServer>(std::vector<std::string>{
+          {"listening_ports", port,
+           "num_threads", "1"
+          }
+      })}
 {
-    mg_exit_library();
+    if (_server->getContext() == nullptr)
+    {
+        throw std::runtime_error{"Creation of the server failed"};
+    }
+    for (auto const& callback : callbacks)
+        _handlers.emplace_back(callback.second);
+    for (size_t i = 0; i < callbacks.size(); ++i)
+        _server->addHandler(callbacks[i].first, &_handlers[i]);
 }
 
 } // namespace serv
