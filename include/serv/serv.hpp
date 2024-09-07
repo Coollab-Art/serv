79925db0d805e0bbdc5b2b96992f33332823d492
Jules Fouchy <jules.fouchy@ntymail.com>
2024-09-07 14:01:20 +0200

    🐛 Send response header when receiving http GET

--- include/serv/serv.hpp
+++ include/serv/serv.hpp
@@ -38,6 +38,13 @@ public:
     auto handleGet(CivetServer*, mg_connection* connection) -> bool override
     {
         _callback(Request{connection});
+        mg_printf(connection,
+                  "HTTP/1.1 200 OK\r\n"
+                  "Content-Type: text/plain\r\n"
+                  "Connection: close\r\n"
+                  "Content-Length:  0\r\n"
+                  "Access-Control-Allow-Origin: *\r\n" // Allow CORS from any origin
+                  "\r\n");
         return true;
     }
 
